797ac01dad6c2b68722de4c1cc6c29d8338cd662
Anthony Izzo <avi224@lehigh.edu>
2020-08-24 16:06:10 -0400

    Change #2

--- Intro to R.R
+++ Intro to R.R
@@ -2,8 +2,8 @@
 # Copy for Anthony Izzo 2020
 ######################################################
 
-# You will see that you can put non-executable "comment" lines
+# Anthony Izzo
 # in your script by putting "#" at the beginning of the lines.
-# 
+# Anthony Izzo
 # In fact, you can put comments anywhere on a line as long as 
 # you put "#" before them.
